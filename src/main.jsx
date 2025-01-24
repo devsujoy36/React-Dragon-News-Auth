6ea4a9e05e52e6859386ef302df1bdf0f7ee767f
unknown <sujoydaspc2023@gmail.com>
2025-01-24 23:56:10 +0600

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+
+      }
+    ]
+    }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
