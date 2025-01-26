61665a6221ac830c6987c4ce770f92e2c66c6f54
Sujoy Das <157239662+devsujoy36@users.noreply.github.com>
2025-01-26 23:30:58 +0600

    Update App.jsx text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className=''>Hello</h1>
+      <h1 className=''>Bari chole aschi tai pc nai code korte partachi na but coding er class dekhtasi learning cholche cholbe</h1>
     </>
   )
 }
