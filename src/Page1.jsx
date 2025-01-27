79dccb21c49f702c33e15efa80acd91861cdbe00
Sujoy Das <157239662+devsujoy36@users.noreply.github.com>
2025-01-27 23:40:50 +0600

    Update Page1.jsx

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,1 +1,8 @@
-
+function Page1() {
+  return (
+    <>
+      <h1 className=''>Bari chole aschi tai pc nai code korte partachi na but coding er class dekhtasi learning cholche cholbe</h1>
+    </>
+  )
+}
+export default Page1
